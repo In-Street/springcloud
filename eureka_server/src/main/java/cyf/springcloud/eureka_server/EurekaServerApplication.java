cb55bf2a005a2e14a629fd1b9d8442dcddc7f7da
chengyufei <chengyufei@daishumovie.com>
2017-08-18 17:57:01 +0800

    start

---
+++ eureka_server/src/main/java/cyf/springcloud/eureka_server/EurekaServerApplication.java
@@ -0,0 +1,12 @@
+package cyf.springcloud.eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
