5ec0a7a3957f8799cd169cd0c9773f34f5aae991
chengyufei <chengyufei@daishumovie.com>
2017-08-29 10:10:40 +0800

    注册中心 与 服务调用者

---
+++ cloud_consumer/src/main/java/cyf/springcloud/consumer/controller/ConsumerController.java
@@ -0,0 +1,26 @@
+package cyf.springcloud.consumer.controller;
+
+import cyf.springcloud.consumer.inter.Remote;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * consumer
+ *
+ * @author Cheng Yufei
+ * @create 2017-08-25 15:24
+ **/
+@RestController
+@RequestMapping("/consumer")
+public class ConsumerController {
+
+    @Autowired
+    Remote remote;
+
+    @RequestMapping("/oneConsumer")
+    public String oneConsumer() {
+        System.out.println();
+        return remote.prducerOne();
+    }
+}
---
+++ cloud_consumer/src/main/java/cyf/springcloud/consumer/inter/Remote.java
@@ -0,0 +1,16 @@
+package cyf.springcloud.consumer.inter;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+//name:远程服务名，及spring.application.name配置的名称
+@FeignClient(name = "cloud-producer")
+public interface Remote {
+
+    //和远程服务中contoller中的方法名和参数需保持一致。
+    @RequestMapping(value = "/producer/oneProducer",method = RequestMethod.GET)
+    public String prducerOne();
+
+}
