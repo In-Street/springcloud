87d70bf570d763dbd3f9bf4fb2cd12cda5c28f22
chengyufei <chengyufei@daishumovie.com>
2017-10-13 15:10:49 +0800

    hystrix 使用 注释添加

--- cloud_consumer/src/main/java/cyf/springcloud/consumer/inter/RemoteHystrix.java
+++ cloud_consumer/src/main/java/cyf/springcloud/consumer/inter/RemoteHystrix.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 /**
  * Hystrix
  *
+ *  一旦后端服务不可用, 断路器会直接切断请求链, 避免发送大量无效请求影响系统吞吐量
+ *
  * @author Cheng Yufei
  * @create 2017-09-01 11:34
  **/
@@ -13,6 +15,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class RemoteHystrix implements Remote{
     @Override
     public String hello(@PathVariable(value = "name") String name) {
-        return "message file"+name;
+        return "message file  "+name;
     }
 }
