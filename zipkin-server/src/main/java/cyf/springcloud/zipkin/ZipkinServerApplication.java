66949aac8bf0bc5fc422e60b9d79548f6d9ec0e6
chengyufei <chengyufei@daishumovie.com>
2017-11-28 17:27:09 +0800

    zipkin mysql数据存储

--- zipkin-server/src/main/java/cyf/springcloud/zipkin/ZipkinServerApplication.java
+++ zipkin-server/src/main/java/cyf/springcloud/zipkin/ZipkinServerApplication.java
@@ -3,13 +3,23 @@ package cyf.springcloud.zipkin;
 import org.springframework.beans.factory.support.DefaultBeanNameGenerator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
 import zipkin.server.EnableZipkinServer;
+import zipkin.storage.mysql.MySQLStorage;
+
+import javax.sql.DataSource;
+
 
 /**
  */
 @SpringBootApplication
 @EnableZipkinServer
 public class ZipkinServerApplication {
+
+    @Bean
+    public MySQLStorage mySQLStorage(DataSource datasource) {
+        return MySQLStorage.builder().datasource(datasource).executor(Runnable::run).build();
+    }
 
     public static void main(String[] args) {
 
